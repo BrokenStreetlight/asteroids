2792ff812433f23d70d467f756a603eb361f6a6f
BrokenStreetlight <adam.clark1213@gmail.com>
2025-03-15 19:21:15 -0400

    moving

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ from constants import (
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS,
 )
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=000)
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        # limit framerate
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
